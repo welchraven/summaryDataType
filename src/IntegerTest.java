20522e02200104ed1c0b6cc036e7660458959855
welchraven <106184854+welchraven@users.noreply.github.com>
2022-05-26 17:28:53 -0400

    Create IntegerTest.java

---
+++ src/IntegerTest.java
@@ -0,0 +1,18 @@
+
+/** class name: IntegerTest.java
+* author: Raven Welch
+* date: 5/26/2022
+* description: Testing int range and overflow 
+*
+*/
+
+public class IntegerTest
+{
+  public static void main(String[] args)
+  {
+    int number = 2147483647;
+  System.out.print(number + 2);
+  
+  
+  }
+}
