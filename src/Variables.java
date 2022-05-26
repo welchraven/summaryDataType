24cf4338ce849cd9dc91284588899fe4b41165f7
welchraven <106184854+welchraven@users.noreply.github.com>
2022-05-26 14:57:35 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -10,11 +10,36 @@
  *            double, boolean, char (2 lines per each type)
  *
  */
-public class Variables
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Summary of The Primitive Data Types");
-    }
 
-}
+        public class Variables
+        {
+            public static void main(String[] args)
+            {
+                System.out.println("Summary of The Primitive Data Types");
+                System.out.println();
+                System.out.print("The byte data type is made up of 8-bits.");
+                System.out.println("The byte can be used to save memory and to clarify your code as documentation.");
+                System.out.println("");
+                System.out.print("She short data is 16-bits.");
+                System.out.println("The short data is also usable for saving memory.");
+                System.out.println("");
+                System.out.print("Int data is made up of 32-bits.");
+                System.out.println("In Java, int can be unsigned with a minimum value of 0.");
+                System.out.println("");
+                System.out.print("The long data type is 64-bits.");
+                System.out.println("Use this data type when you need a value wider than int.");
+                System.out.println("");
+                System.out.print("The float data type is a single-precision.");
+                System.out.println("Use float instead of double to save large memory.");
+                System.out.println("");
+                System.out.print("The double data type is a double-precision.");
+                System.out.println("This data type should not be used for precise values like currency.");
+                System.out.println("");
+                System.out.print("Boolean only has two values of true and false.");
+                System.out.println("Use this value for only true/false conditions not for size.");
+                System.out.println("");
+                System.out.print("Char is a single 16-bit unicode.");
+                System.out.println("Has a minimum value of 0 and a maximum value of 65,535.");
+
+            }
+        }
